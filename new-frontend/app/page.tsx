4d45c3ed79055b9e60bbec65f1c76d02c78759cd
isekaisaru <isekaisaru@gmail.com>
2024-03-28 06:08:10 +0900

    new-frontendディレクトリを新規作成してshadcn/uiでのスタイルを適用させていく

---
+++ new-frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="p-24">
+      <Button>夢の記録</Button>
+    </main>
+  );
+}
