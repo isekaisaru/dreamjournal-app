5d1ea34e4f93bba069e66829a4a2b223fea04d56
isekaisaru <isekaisaru@gmail.com>
2024-03-30 06:29:06 +0900

    夢の記録ページの雛形作成する

---
+++ new-frontend/app/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="py-2 px-4 border-t">
+        <small>@2024 dream-journal-app</small>
+      </footer></div>
+  )
+}
+
+export default Footer
