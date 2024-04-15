c605901212d792c9d2c90910bc71b7f4a421469d
isekaisaru <isekaisaru@gmail.com>
2024-04-16 04:44:47 +0900

    ローディング、エラーページの作成

---
+++ frontend/components/DreamCard.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Dream } from '@/app/types';
+import Link from 'next/link';
+
+type DreamCardProps = {
+  dream: Dream;
+}
+
+const DreamCard = ({ dream }: DreamCardProps) => {
+  return (
+    <div className="shadow my-4 flex flex-col" key={dream.id}>
+          <Link href={`dream/${dream.id}`} className="text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4">
+            {dream.title}
+          </Link>
+          <Link href={`dream/${dream.id}`} className="text-slate-900 text-xl font-bold hover:text-gray-700 pb-4">
+            {dream.description.length > 70 
+            ? dream.description.substring(0,70) + "..."
+            : dream.description}
+          </Link>
+          <Link href={`dream/${dream.id}`} className="text-pink-800 hover:text-black">
+            続きを読む
+          </Link>
+        </div>
+  )
+};
+
+export default DreamCard;
--- frontend/components/DreamList.tsx
+++ frontend/components/DreamList.tsx
@@ -1,6 +1,7 @@
 import { Dream } from '@/app/types';
 import Link from 'next/link';
 import React from 'react';
+import DreamCard from './DreamCard';
 
 type DreamListProps = {
    dreams: Dream[];
@@ -10,14 +11,7 @@ const DreamList = ({ dreams }: DreamListProps) => {
   return (
     <div>
       {dreams.map((dream) => (
-        <div className="shadow my-4 flex flex-col" key={dream.id}>
-          <Link href={`dream/${dream.id}`} className="text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4">
-            {dream.title}
-          </Link>
-          <Link href={`dream/${dream.id}`} className="text-slate-900 text-xl font-bold hover:text-gray-700 pb-4">
-            {dream.description}
-          </Link>
-        </div>
+        <DreamCard dream={dream} key={dream.id} />
        ))}  
 
     </div>
