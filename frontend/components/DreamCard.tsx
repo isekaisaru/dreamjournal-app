b44ffd962bc52a02533df76179b2511384f04951
isekaisaru <isekaisaru@gmail.com>
2024-04-27 04:33:59 +0900

    日付表示機能を追加

--- frontend/components/DreamCard.tsx
+++ frontend/components/DreamCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Dream } from '@/app/types';
 import Link from 'next/link';
+import { format } from 'date-fns';
 
 type DreamCardProps = {
   dream: Dream;
@@ -12,6 +13,9 @@ const DreamCard = ({ dream }: DreamCardProps) => {
           <Link href={`dream/${dream.id}`} className="text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4">
             {dream.title}
           </Link>
+          <p className="text-gray-600">
+            {format(new Date(dream.created_at), 'yyyy-MM-dd')}
+          </p>
           <Link href={`dream/${dream.id}`} className="text-slate-900 text-xl font-bold hover:text-gray-700 pb-4">
             {dream.description.length > 70 
             ? dream.description.substring(0,70) + "..."
