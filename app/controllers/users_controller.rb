1ba20198edf3fb0567f286eacbb64bec8f44aef3
isekaisaru <isekaisaru@gmail.com>
2024-05-17 05:41:06 +0900

    ユーザー登録のためのルーティングとuserコントローラーを作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+  def register
+
+    @user = User.new(user_params)
+
+    if @user.save
+      token = encode_token({ user_id: @user.id })
+
+      render json: { user: @user, token: token }, status: :created
+      
+    else
+       
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
+  def encode_token(payload)
+
+    JWT.encode(payload, 'your_secret_key')
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
    resources :dreams, only: [:index, :show, :create, :update, :destroy]
 
     post '/login', to: 'sessions#create'
+    
+    post 'register', to: 'users#register'
 end
