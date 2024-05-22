6120e67b8a5b99f456fd7d87c55cbc2b51dfc92f
isekaisaru <isekaisaru@gmail.com>
2024-05-23 05:05:53 +0900

    ユーザー名のマイグレーションファイル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :email, presence: turu, uniqueness: true
+  validates :username, presence: turu, uniqueness: true
+  validates :password, presence: turu, length: { minimum: 6 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_12_194624) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_19_205627) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_12_194624) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "username"
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "dreams", "users"
