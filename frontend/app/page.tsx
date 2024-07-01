c75aafe332f1cf7e43304f7fc07e7f3f749af5e9
isekaisaru <isekaisaru@gmail.com>
2024-07-02 06:17:56 +0900

    ユーザー登録なしでも夢の記録ができる機能を実装

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,61 +1,27 @@
+// app/page.tsx
 "use client";
 
-import { useEffect, useState } from "react";
-import DreamList from "@/components/DreamList";
-import SearchBar from "@/components/SearchBar";
-import Link from "next/link";
-import axios from "axios";
-import { Dream } from "@/app/types";
+import useAuth from "@/hooks/useAuth";
+import HomePage from "./home/page";
+import TrialPage from "./trial/page";
 
-export default function Home() {
-  const [dreams, setDreams] = useState<Dream[]>([]);
 
-    const fetchDreams = async (query = '', startDate = '', endDate = '') => {
-      try {
-        const response = await axios.get('http://localhost:3001/dreams', {
-          params: { query, start_date: startDate, end_date: endDate },
-        });
-        setDreams(response.data);
-      } catch (error) {
-        console.error('Error fetching dreams:', error);
-      }
-    };
-useEffect(() => {
-    fetchDreams();
-  }, []);
+export default function IndexPage() {
+  try {
+    const isAuthenticated = useAuth();
+    console.log("isAuthenticated in IndexPage:", isAuthenticated);
 
-  return (
-    <div className="md:flex">
-      <section className="w-full md:w-2/3 flex flex-col items-center px-3 md:px-6">
-        <SearchBar onSearch={fetchDreams}/>
-        <DreamList dreams={dreams} />
-      </section>
-
-      <aside className="w-full md:w-1/3 flex flex-col items-center px-3 md:px-6 mt-4 md:mt-0">
-        <div className="bg-white shadow-md rounded p-4 mb-6">
-          <h3 className="font-bold text-gray-600 mb-2">前に見た夢</h3>
-          <p className="text-gray-600">
-            前に見た夢を振り返ってみましょう！
-          </p>
-        </div>
-        <ul className="space-y-2">
-          <li>
-            <Link href="/dream/1" className="text-gray-500 hover:underline">
-              2024年1月の夢
-            </Link>
-          </li>
-          <li>
-            <Link href="/dream/2" className="text-blue-500 hover:underline">
-              2024年2月の夢
-            </Link>
-          </li>
-          <li>
-            <Link href="/dream/3" className="text-blue-500 hover:underline">
-              2024年3月の夢
-            </Link>
-          </li>
-        </ul>
-      </aside>
-    </div>
-  );
+    if (isAuthenticated) {
+      return <HomePage />;
+    } else {
+      return <TrialPage />;
+    }
+  } catch (error) {
+    console.error("Error in IndexPage:", error);
+    if (error instanceof Error) {
+      return <div>Error: {error.message}</div>;
+    } else {
+      return <div>Unknown error occurred</div>;
+    }
+  }
 }
