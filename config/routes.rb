bf19fea00157e0a4139b35891475b70715149b2c
isekaisaru <isekaisaru@gmail.com>
2024-08-23 04:34:32 +0900

    アプリケーションが正常に立ち上がったので変更をコミットしました。

--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-    resources :dreams, only: [:index, :show, :create, :update, :destroy]
-    get 'my_dreams', to: 'dreams#my_dreams'
-    post 'login', to: 'sessions#create'
-    post 'register', to: 'users#register'
-    post 'trial_users', to: 'trial_users#create'
-end
--- db/migrate/20240625200357_add_trial_user_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddTrialUserToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :trial_user, :boolean
-  end
-end
