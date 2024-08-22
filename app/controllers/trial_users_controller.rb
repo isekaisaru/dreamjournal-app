bf12e51d51e19f6fd64b207fc43ab301d71d666a
isekaisaru <isekaisaru@gmail.com>
2024-08-23 04:34:32 +0900

    アプリケーションが正常に立ち上がったので変更をコミットしました。

--- app/controllers/trial_users_controller.rb
+++
@@ -1,7 +0,0 @@
-class TrialUsersController < ApplicationController
-  def create
-    user = User.create(trial_user: true)
-    user.save(validate: false)
-    render json: { user_id: user.id, token: JsonWebToken.encode(user_id: user.id)}, status: :created
-  end
-end
