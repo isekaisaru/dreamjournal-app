dbb58ff44d71bd648526cc9dcc7e1188b3b88529
isekaisaru <isekaisaru@gmail.com>
2024-09-16 04:20:24 +0900

    SessionControllerの修正

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -1,13 +1,24 @@
 class SessionsController < ApplicationController
+  skip_before_action :authorize_request, only: [:create]
    def create
-
     user = User.find_by(email: params[:email])
+    if user
+      Rails.logger.info "User found: #{user.username}"
+    else
+      Rails.logger.warn "No user found with email: #{params[:email]}"
+    end
 
     if user && user.authenticate(params[:password])
       token = encode_token({ user_id: user.id })
-      render json: { user: user, jwt: token }, status: :created
+      render json: { user: user.as_json(only: [:id, :email, :username]), jwt:token }, status: :created
     else
       render json: { error: 'Invalid email or password'}, status: :unauthorized
     end
+   end
+
+   private
+
+   def encode_token(payload)
+     JWT.encode(payload, Rails.application.credentials.secret_key_base)
    end
 end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, unless: :trial_user?
   validates :password, presence: true, length: { minimum: 6 }, unless: :trial_user?
 
-  def trial_user?
-    self.trial_user
+  def generate_jwt
+    JWT.encode({user_id: self.id, exp: 60.days.from_now.to_i}, Rails.application.credentials.secret_key_base)
   end
 end
